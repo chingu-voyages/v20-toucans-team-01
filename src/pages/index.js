642cfa87fc89e5a22fe18ee51afe262619bc8738
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-06-05 03:57:27 -0700

    :bug: charSet typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function App() {
         `}
       >
         <Helmet>
-          <meta charset="UTF-8" />
+          <meta charSet="UTF-8" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0, minimum-scale=1"
