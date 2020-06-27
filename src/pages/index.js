3dd5362b9bee3dbc3dc722274450fe64774b4008
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-06-27 12:53:06 -0700

    Add CSSReset

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,7 @@ export default function App() {
   return (
     <React.StrictMode>
       <StylesProvider injectFirst>
+        <CSSReset />
         <div
           className="App"
           css={css`
