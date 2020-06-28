83f724f94ca898f6b422b1515f61bea239e4239f
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-06-27 20:26:28 -0700

    Add and extend theme defaults

---
+++ src/gatsby-plugin-chakra-ui/theme.js
@@ -0,0 +1,10 @@
+import theme from "@chakra-ui/theme";
+export default {
+  ...theme,
+  fonts: {
+    ...theme.fonts,
+    heading: '"Inter", sans-serif',
+    body: '"Inter", sans-serif',
+    //mono: "Menlo, monospace",
+  },
+};
