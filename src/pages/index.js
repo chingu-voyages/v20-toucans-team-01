cb971c9262b624bdafc56eb37dcbbcc49e5ff652
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-06-27 20:26:51 -0700

    Remove MUI Styles Provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,91 +10,88 @@ import "../styles/global.css";
 import Difficulty from "../components/Difficulty";
 import Mealtype from "../components/Mealtype";
 import Nav from "../components/Nav";
-import { StylesProvider } from "@material-ui/core/styles";
 
 export default function App() {
   const recipes = useRecipes();
 
   return (
     <React.StrictMode>
-      <StylesProvider injectFirst>
-        <div
-          css={css`
-            margin: 0 auto;
-            text-align: center;
-            a {
-              text-decoration: none;
-            }
-          `}
-        >
-          <Helmet>
-            <meta charSet="UTF-8" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1.0, minimum-scale=1"
-            />
-            <title>Toucan Recipes</title>
-            <link
-              rel="icon"
-              href='data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="12 0 100 100"><text y=".9em" font-size="90">🥘</text></svg>'
-            />
-          </Helmet>
-          {/* Main page starts here */}
+      <div
+        css={css`
+          margin: 0 auto;
+          text-align: center;
+          a {
+            text-decoration: none;
+          }
+        `}
+      >
+        <Helmet>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, minimum-scale=1"
+          />
+          <title>Toucan Recipes</title>
+          <link
+            rel="icon"
+            href='data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="12 0 100 100"><text y=".9em" font-size="90">🥘</text></svg>'
+          />
+        </Helmet>
+        {/* Main page starts here */}
 
-          {/* Nav Bar starts here */}
-          <Nav />
+        {/* Nav Bar starts here */}
+        <Nav />
 
-          {/* Nav Bar ends here */}
+        {/* Nav Bar ends here */}
 
-          {/* First section starts here*/}
+        {/* First section starts here*/}
 
+        <div>
           <div>
-            <div>
-              <h2>Weelky chief recommendation</h2>
-            </div>
-            <div>
-              {recipes.map((recipe) => (
-                <RecipePreview key={recipe.slug} recipe={recipe} />
-              ))}
-            </div>
+            <h2>Weelky chief recommendation</h2>
           </div>
-          {/* First section ends here */}
-
-          {/* Second section starts here*/}
           <div>
-            <div>
-              <h2>Categories</h2>
-            </div>
-            <div>
-              {recipes.map((recipe) => (
-                <RecipePreview key={recipe.slug} recipe={recipe} />
-              ))}
-            </div>
-            <div>
-              {recipes.map((recipe) => (
-                <RecipePreview key={recipe.slug} recipe={recipe} />
-              ))}
-            </div>
-            <div>
-              {recipes.map((recipe) => (
-                <RecipePreview key={recipe.slug} recipe={recipe} />
-              ))}
-            </div>
+            {recipes.map((recipe) => (
+              <RecipePreview key={recipe.slug} recipe={recipe} />
+            ))}
           </div>
-          {/* Second section ends here */}
-
-          {/* Footer starts here */}
-          <footer>
-            <div>
-              © 2020 Copyright{" "}
-              <a href="https://github.com/chingu-voyages/v20-toucans-team-01">
-                Voyage 20 Toucans Team 01
-              </a>
-            </div>
-          </footer>
-          {/* Footer ends here */}
         </div>
-      </StylesProvider>
+        {/* First section ends here */}
+
+        {/* Second section starts here*/}
+        <div>
+          <div>
+            <h2>Categories</h2>
+          </div>
+          <div>
+            {recipes.map((recipe) => (
+              <RecipePreview key={recipe.slug} recipe={recipe} />
+            ))}
+          </div>
+          <div>
+            {recipes.map((recipe) => (
+              <RecipePreview key={recipe.slug} recipe={recipe} />
+            ))}
+          </div>
+          <div>
+            {recipes.map((recipe) => (
+              <RecipePreview key={recipe.slug} recipe={recipe} />
+            ))}
+          </div>
+        </div>
+        {/* Second section ends here */}
+
+        {/* Footer starts here */}
+        <footer>
+          <div>
+            © 2020 Copyright{" "}
+            <a href="https://github.com/chingu-voyages/v20-toucans-team-01">
+              Voyage 20 Toucans Team 01
+            </a>
+          </div>
+        </footer>
+        {/* Footer ends here */}
+      </div>
     </React.StrictMode>
   );
 }
