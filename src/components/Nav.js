b69ec11e70a34006ba29c9012109874cf9683397
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-06-27 20:51:35 -0700

    Migrate Nav to Chakra

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,44 +1,64 @@
 import React from "react";
 import "../styles/bootstrap.min.css";
 import "../styles/global.css";
-import { Button } from "@chakra-ui/core";
+import {
+  Box,
+  Heading,
+  Flex,
+  Link,
+  InputGroup,
+  InputLeftElement,
+  Stack,
+  Input,
+  Icon,
+} from "@chakra-ui/core";
+import { Link as GatsbyLink } from "gatsby";
+import { MdSearch } from "react-icons/md";
+
+const MenuLink = ({ children }) => (
+  <Link as={GatsbyLink} mt={{ base: 4, md: 0 }} mr={6} display="block">
+    {children}
+  </Link>
+);
 
 const Nav = () => {
   return (
-    <nav>
-      <Button
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span></span>
-      </Button>
-      <div>
-        <ul>
-          <li>
-            <a>Breakfast</a>
-          </li>
-          <li>
-            <a>Brunch</a>
-          </li>
-          <li>
-            <a>Lunch</a>
-          </li>
-          <li>
-            <a>Dinner</a>
-          </li>
-        </ul>
-      </div>
-      <h1>Toucan Recipes</h1>
+    <Flex
+      as="nav"
+      align="center"
+      justify={{ xs: "center", md: "space-between" }}
+      wrap="wrap"
+      padding="1.5rem"
+      bg="teal.500"
+      color="white"
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg">
+          Toucan Recipes
+        </Heading>
+      </Flex>
 
-      <form>
-        <input type="search" placeholder="Search" aria-label="Search"></input>
-        <Button>Search</Button>
-      </form>
-    </nav>
+      <Box
+        display={{ xs: "none", md: "flex" }}
+        width={{ xs: "full", md: "auto" }}
+        alignItems="center"
+        flexGrow={1}
+      >
+        <MenuLink>Breakfast</MenuLink>
+        <MenuLink>Brunch</MenuLink>
+        <MenuLink>Lunch</MenuLink>
+        <MenuLink>Dinner</MenuLink>
+      </Box>
+
+      <Stack spacing={4}>
+        <InputGroup>
+          <InputLeftElement
+            children={<Icon as={MdSearch} name="search" color="gray.300" />}
+          />
+          <Input color="black" type="search" placeholder="Search" />
+        </InputGroup>
+      </Stack>
+    </Flex>
   );
 };
 
