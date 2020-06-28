2ae2fb1c6e5b4457992068830acb8390e14fcd63
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-06-27 18:11:07 -0700

    Remove all classNames

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,9 +5,8 @@ import { Button } from "@chakra-ui/core";
 
 const Nav = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav>
       <Button
-        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarNavDropdown"
@@ -15,44 +14,29 @@ const Nav = () => {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon"></span>
+        <span></span>
       </Button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Breakfast
-            </a>
+      <div>
+        <ul>
+          <li>
+            <a>Breakfast</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Brunch
-            </a>
+          <li>
+            <a>Brunch</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Lunch
-            </a>
+          <li>
+            <a>Lunch</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Dinner
-            </a>
+          <li>
+            <a>Dinner</a>
           </li>
         </ul>
       </div>
       <h1>Toucan Recipes</h1>
 
-      <form className="form-inline">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        ></input>
-        <Button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </Button>
+      <form>
+        <input type="search" placeholder="Search" aria-label="Search"></input>
+        <Button>Search</Button>
       </form>
     </nav>
   );
