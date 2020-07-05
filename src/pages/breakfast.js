90dcd4de599b13c0c8574acb3c58749106ba6b5d
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-07-05 00:34:45 -0700

    demo breakfast page

---
+++ src/pages/breakfast.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import useRecipes from "../hooks/use-recipes";
+import RecipePreview from "../components/recipe-preview";
+import Nav from "../components/Nav";
+import {
+  SimpleGrid,
+  Flex,
+  Heading,
+  Text,
+  Box,
+  Link,
+  VStack,
+} from "@chakra-ui/core";
+
+export default function Breakfast() {
+  const recipes = useRecipes();
+
+  return (
+    <React.StrictMode>
+      <Helmet>
+        <html lang="en" />
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1"
+        />
+        <title>Breakfast | Toucan Recipes</title>
+        <link
+          rel="icon"
+          href='data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="12 0 100 100"><text y=".9em" font-size="90">🥘</text></svg>'
+        />
+      </Helmet>
+
+      <Nav />
+
+      {/* Main Content */}
+      <Box as="main">
+        {/* First Section*/}
+        <VStack as="section" spacing={5}>
+          <Heading fontFamily="aleo, monospace">Featured</Heading>
+          <Text textAlign="center">
+            Feeling hungry? Enjoy these recipes curated weekly by a chef lead!
+          </Text>
+          <SimpleGrid spacing={10} columns={{ xs: 1, md: 2 }} mb={5}>
+            {recipes.map((recipe) => (
+              <RecipePreview
+                display={
+                  recipe.type.toLowerCase?.() == "breakfast" ? "unset" : "none"
+                }
+                key={recipe.slug}
+                recipe={recipe}
+              />
+            ))}
+          </SimpleGrid>
+        </VStack>
+      </Box>
+
+      {/* Footer*/}
+      <Flex as="footer" justify="center" mt={4}>
+        <Text>
+          © 2020 Copyright{" "}
+          <Link
+            isExternal
+            href="https://github.com/chingu-voyages/v20-toucans-team-01"
+            color="facebook.500"
+          >
+            Voyage 20 Toucans Team 01
+          </Link>
+        </Text>
+      </Flex>
+    </React.StrictMode>
+  );
+}
