d75665b33346b0e203e0fa3739dde84c9315bf12
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-07-08 05:53:12 -0700

    source locally meal type images

--- src/components/MealTypeBtn.js
+++ src/components/MealTypeBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@chakra-ui/core";
 
-export default function MealTypeBtn({ children, src, ...props }) {
+export default function MealTypeBtn({ children, fluid, ...props }) {
   return (
     <Button
       display="flex"
@@ -11,13 +11,13 @@ export default function MealTypeBtn({ children, src, ...props }) {
       boxSize={{ base: "40vw", md: "20vw" }}
       maxW={64}
       maxH={64}
-      bg={`url("${src}") rgba(0, 0, 0, 0.4)`}
+      bg={`url("${fluid.srcWebp}") rgba(0, 0, 0, 0.4)`}
       bgBlendMode="multiply"
       color="white"
       fontFamily="aleo"
       fontSize={24}
       textShadow="rgba(0, 0, 0, 0.16) 0px 0.125rem 0.5rem"
-      _hover={{ color: "black", background: "#E2E8F0" }}
+      _hover={{ color: "black", background: `url("${fluid.tracedSVG}")` }}
       {...props}
     >
       {children}
--- src/components/meal-type-group.js
+++ src/components/meal-type-group.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { SimpleGrid } from "@chakra-ui/core";
 import MealTypeBtn from "./MealTypeBtn";
+import { useStaticQuery, graphql } from "gatsby";
 
 export default function MealTypes() {
+  var images = useStaticQuery(graphql`
+    fragment getImg on File {
+      sharp: childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+    query {
+      breakfastImg: file(relativePath: { eq: "breakfast.png" }) {
+        ...getImg
+      }
+      brunchImg: file(relativePath: { eq: "brunch.png" }) {
+        ...getImg
+      }
+      lunchImg: file(relativePath: { eq: "lunch.png" }) {
+        ...getImg
+      }
+      dinnerImg: file(relativePath: { eq: "dinner.png" }) {
+        ...getImg
+      }
+    }
+  `);
   return (
     <SimpleGrid spacing={10} columns={{ xs: 2, md: 4 }} mb={5}>
-      <MealTypeBtn src="images/pancakes.png">Breakfast</MealTypeBtn>
-      <MealTypeBtn src="images/tamagoyaki.png">Brunch</MealTypeBtn>
-      <MealTypeBtn src="images/clam_chowder.png">Lunch</MealTypeBtn>
-      <MealTypeBtn src="images/rosol.png">Dinner</MealTypeBtn>
+      <MealTypeBtn fluid={images.breakfastImg.sharp.fluid}>
+        Breakfast
+      </MealTypeBtn>
+      <MealTypeBtn fluid={images.brunchImg.sharp.fluid}>Brunch</MealTypeBtn>
+      <MealTypeBtn fluid={images.lunchImg.sharp.fluid}>Lunch</MealTypeBtn>
+      <MealTypeBtn fluid={images.dinnerImg.sharp.fluid}>Dinner</MealTypeBtn>
     </SimpleGrid>
   );
 }
