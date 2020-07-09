8a2a5a901522f939e3b12545e2848a822ed2014d
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-07-08 21:25:05 -0700

    add divider between ingredients and steps

--- src/components/recipe-layout.js
+++ src/components/recipe-layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@chakra-ui/core";
+import { Box, Divider } from "@chakra-ui/core";
 
 export default function RecipeLayout({ children }) {
   var sliceIndex = children.findIndex((el) => el.props.mdxType == "ol");
@@ -9,6 +9,7 @@ export default function RecipeLayout({ children }) {
         <h2>Ingredients</h2>
         {children.slice(0, sliceIndex)}
       </Box>
+      <Divider />
       <Box>
         <h2>Steps</h2>
         {children.slice(sliceIndex)}
