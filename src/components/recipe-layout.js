2044cb4a6914ed61bba832b4bf8d472756eda538
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-07-09 00:19:34 -0700

    remove layout, enhance recipe page styles

--- src/components/recipe-layout.js
+++ src/components/recipe-layout.js
@@ -1,20 +1,39 @@
 import React from "react";
 import { Box, Divider } from "@chakra-ui/core";
+import { css } from "@emotion/core";
 
 export default function RecipeLayout({ children }) {
   var sliceIndex = children.findIndex((el) => el.props.mdxType == "ol");
   return sliceIndex ? (
-    <>
+    <Box
+      css={css`
+        ul,
+        ol {
+          padding-left: 1.2rem;
+        }
+        li {
+          margin-top: 0.25rem;
+        }
+        h1,
+        h2,
+        h3 {
+          font-family: "aleo", "Courier New", Courier, monospace;
+          font-size: 1.5rem;
+          font-weight: 600;
+        }
+        max-width: 80vw;
+      `}
+    >
       <Box>
         <h2>Ingredients</h2>
         {children.slice(0, sliceIndex)}
       </Box>
-      <Divider />
+      <Divider marginY={5} />
       <Box>
         <h2>Steps</h2>
         {children.slice(sliceIndex)}
       </Box>
-    </>
+    </Box>
   ) : (
     <>{children}</>
   );
--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -2,7 +2,6 @@ import React from "react";
 import { graphql, Link as GatsbyLink } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { Helmet } from "react-helmet";
-import Layout from "../styles/layout";
 import { MDXProvider } from "@mdx-js/react";
 import RecipeLayout from "../components/recipe-layout";
 import Nav from "../components/Nav";
@@ -18,7 +17,6 @@ import {
 import { IoMdTime } from "react-icons/io";
 import { FaFlag } from "react-icons/fa";
 import { GiChefToque } from "react-icons/gi";
-
 export const query = graphql`
   query($slug: String!) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
@@ -44,39 +42,37 @@ export default function RecipeTemplate({
   return (
     <Box>
       <Nav />
-      <Layout>
-        <Helmet>
-          <title>Cook {title} — Toucan Recipes</title>
-        </Helmet>
+      <Helmet>
+        <title>Cook {title} — Toucan Recipes</title>
+      </Helmet>
 
-        <VStack>
-          <Box>
-            <Heading>{title}</Heading>
-            <HStack isInline>
-              <Tag size="md" variant="outline" colorScheme="red">
-                <TagLeftIcon as={IoMdTime} mr={1} />
-                {type[0].toUpperCase() + type.substring(1)}
-              </Tag>
-              <Tag size="md" colorScheme="gray">
-                <TagLeftIcon as={GiChefToque} mr={1} />
-                {difficulty[0].toUpperCase() + difficulty.substring(1)}
-              </Tag>
-              <Tag size="md" variant="solid" colorScheme="blue">
-                <TagLeftIcon as={FaFlag} mr={1} />
-                {category[0].toUpperCase() + category.substring(1)}
-              </Tag>
-            </HStack>
+      <VStack>
+        <Heading>{title}</Heading>
+        <HStack isInline>
+          <Tag size="md" variant="outline" colorScheme="red">
+            <TagLeftIcon as={IoMdTime} mr={1} />
+            {type[0].toUpperCase() + type.substring(1)}
+          </Tag>
+          <Tag size="md" colorScheme="gray">
+            <TagLeftIcon as={GiChefToque} mr={1} />
+            {difficulty[0].toUpperCase() + difficulty.substring(1)}
+          </Tag>
+          <Tag size="md" variant="solid" colorScheme="blue">
+            <TagLeftIcon as={FaFlag} mr={1} />
+            {category[0].toUpperCase() + category.substring(1)}
+          </Tag>
+        </HStack>
 
-            <MDXProvider components={{ RecipeLayout }}>
-              <MDXRenderer>{body}</MDXRenderer>
-            </MDXProvider>
+        <Box mt={5}>
+          <MDXProvider components={{ RecipeLayout }}>
+            <MDXRenderer>{body}</MDXRenderer>
+          </MDXProvider>
+        </Box>
 
-            <Link as={GatsbyLink} to="/">
-              &larr; back to home
-            </Link>
-          </Box>
-        </VStack>
-      </Layout>
+        <Link as={GatsbyLink} to="/">
+          &larr; back to home
+        </Link>
+      </VStack>
     </Box>
   );
 }
