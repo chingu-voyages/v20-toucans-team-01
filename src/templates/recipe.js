7250794291fbf107759f3aabe5d073761dc90a9f
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-07-08 22:03:57 -0700

    add some tags

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -6,7 +6,18 @@ import Layout from "../styles/layout";
 import { MDXProvider } from "@mdx-js/react";
 import RecipeLayout from "../components/recipe-layout";
 import Nav from "../components/Nav";
-import { Link, VStack, Box, Heading, Tag, HStack } from "@chakra-ui/core";
+import {
+  Link,
+  VStack,
+  Box,
+  Heading,
+  Tag,
+  HStack,
+  TagLeftIcon,
+} from "@chakra-ui/core";
+import { IoMdTime } from "react-icons/io";
+import { FaFlag } from "react-icons/fa";
+import { GiChefToque } from "react-icons/gi";
 
 export const query = graphql`
   query($slug: String!) {
@@ -42,13 +53,16 @@ export default function RecipeTemplate({
           <Box>
             <Heading>{title}</Heading>
             <HStack isInline>
-              <Tag size="sm" variant="outline" colorScheme="red">
+              <Tag size="md" variant="outline" colorScheme="red">
+                <TagLeftIcon as={IoMdTime} mr={1} />
                 {type[0].toUpperCase() + type.substring(1)}
               </Tag>
-              <Tag size="sm" colorScheme="gray">
+              <Tag size="md" colorScheme="gray">
+                <TagLeftIcon as={GiChefToque} mr={1} />
                 {difficulty[0].toUpperCase() + difficulty.substring(1)}
               </Tag>
-              <Tag size="sm" variant="solid" colorScheme="blue">
+              <Tag size="md" variant="solid" colorScheme="blue">
+                <TagLeftIcon as={FaFlag} mr={1} />
                 {category[0].toUpperCase() + category.substring(1)}
               </Tag>
             </HStack>
