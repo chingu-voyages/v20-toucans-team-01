bc3825298cd239fac052c2d88b1d0061323513a8
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-07-10 16:37:29 -0700

    refactor: shorten mealtypegroup

--- src/components/meal-type-group.js
+++ src/components/meal-type-group.js
@@ -29,30 +29,15 @@ export default function MealTypes({ getTypeHandler, type }) {
   `);
   return (
     <SimpleGrid spacing={10} columns={{ xs: 2, md: 4 }} mb={5}>
-      <MealTypeBtn
-        getTypeHandler={getTypeHandler}
-        btnType="breakfast"
-        type={type}
-        fluid={images.breakfastImg.sharp.fluid}
-      />
-      <MealTypeBtn
-        getTypeHandler={getTypeHandler}
-        btnType="brunch"
-        type={type}
-        fluid={images.brunchImg.sharp.fluid}
-      />
-      <MealTypeBtn
-        getTypeHandler={getTypeHandler}
-        btnType="lunch"
-        type={type}
-        fluid={images.lunchImg.sharp.fluid}
-      />
-      <MealTypeBtn
-        getTypeHandler={getTypeHandler}
-        btnType="dinner"
-        type={type}
-        fluid={images.dinnerImg.sharp.fluid}
-      />
+      {["breakfast", "brunch", "lunch", "dinner"].map((btnType) => (
+        <MealTypeBtn
+          key={btnType}
+          btnType={btnType}
+          getTypeHandler={getTypeHandler}
+          type={type}
+          fluid={images[`${btnType}Img`].sharp.fluid}
+        />
+      ))}
     </SimpleGrid>
   );
 }
