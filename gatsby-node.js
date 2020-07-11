2ad8e357536f3c6a6b30c8193455919f6e43f195
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-07-10 23:29:28 -0700

    :bug: fix search indexing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,7 @@
-
 // Lunr Search Setup
-const { GraphQLJSONObject } = require('graphql-type-json')
-const striptags = require('striptags')
-const lunr = require('lunr');
+const { GraphQLJSONObject } = require("graphql-type-json");
+const striptags = require("striptags");
+const lunr = require("lunr");
 
 exports.createResolvers = ({ cache, createResolvers }) => {
   createResolvers({
@@ -11,59 +10,60 @@ exports.createResolvers = ({ cache, createResolvers }) => {
         type: GraphQLJSONObject,
         resolve: (source, args, context, info) => {
           const recipeNodes = context.nodeModel.getAllNodes({
-            type: 'allMdx',
-          })
-          const type = info.schema.getType('allMdx')
-          return createIndex(recipeNodes, type, cache)
+            type: "Mdx",
+          });
+          const type = info.schema.getType("Mdx");
+          return createIndex(recipeNodes, type, cache);
         },
       },
     },
-  })
-}
+  });
+};
 
 const createIndex = async (recipeNodes, type, cache) => {
-  const cacheKey = 'LunrIndex'
-  const cached = await cache.get(cacheKey)
-  if(cached){
-    return cached
+  const cacheKey = "LunrIndex";
+  const cached = await cache.get(cacheKey);
+  if (cached) {
+    return cached;
   }
-  const documents = []
-  const store = {}
+  const documents = [];
+  const store = {};
   // Iterate over all recipes
-  for(const node of recipeNodes) {
-    const {slug} = node.frontmatter
-    const title = node.frontmatter.title
+  for (const node of recipeNodes) {
+    const { slug } = node.frontmatter;
+    const title = node.frontmatter.title;
     const [html, excerpt] = await Promise.all([
-      type.getFields().html.resolve(node),
+      // MDX uses body as html, MDX html for RSS feeds
+      type.getFields().body.resolve(node),
       type.getFields().excerpt.resolve(node, { pruneLength: 40 }),
-    ])
+    ]);
     // Once html is resolved, add a slug-title-content object to the documents array
     documents.push({
       slug: node.frontmatter.slug,
-      title: node.frontmatter.title, 
+      title: node.frontmatter.title,
       type: node.frontmatter.type,
       difficulty: node.frontmatter.difficulty,
-      content: striptags(html)
-    })
+      content: striptags(html),
+    });
     store[slug] = {
       title,
       excerpt,
-    }
+    };
   }
-  const index = lunr(function() {
-    this.ref('slug')
-    this.field('title')
-    this.field('excerpt')
-    for(const doc of documents) {
-      this.add(doc)
+  const index = lunr(function () {
+    this.ref("slug");
+    this.field("title");
+    this.field("excerpt");
+    for (const doc of documents) {
+      this.add(doc);
     }
-  })
-  const json =  { index: index.toJSON(), store }
-  await cache.set(cacheKey, json)
-  return json
-}
+  });
+  const json = { index: index.toJSON(), store };
+  await cache.set(cacheKey, json);
+  return json;
+};
 
-
+// Recipe pages
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     query {
@@ -97,5 +97,3 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     }
   });
 };
-
-
