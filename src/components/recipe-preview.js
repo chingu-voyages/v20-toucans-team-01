9fd4310d566cf5eddb179153a6416a46a421af22
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-07-11 09:18:02 -0700

    subtler designs

--- src/components/recipe-preview.js
+++ src/components/recipe-preview.js
@@ -9,14 +9,18 @@ export default function RecipePreview({
 }) {
   return (
     <Link
-      borderRadius="10%"
+      borderRadius={10}
       as={GatsbyLink}
       to={`${type}/${difficulty}/${slug}`}
       {...props}
     >
       <Image
         as={GatsbyImage}
-        borderRadius="10%"
+        borderWidth={1}
+        borderStyle="solid"
+        borderColor="gray.300"
+        boxShadow="rgba(0,0,0,0.1) 0px 1px 3px 0px, rgba(0,0,0,0.06) 0px 1px 2px 0px"
+        borderRadius={10}
         fluid={image.sharp.fluid}
         alt={`Cooked ${title}`}
         boxSize={64}
